ee810cc167927cc8e1bdb9a54a7a4207ca217f8d
abigaelbousquet <truthbousquet@gmail.com>
2024-04-19 14:25:21 -0400

    feat: attempted new deserializing of instructions, BROKEN (tried to deserialize meal instructions json into Set<String> to then deserialize the single recipe strings, failed)

--- server/src/main/java/Parsing/Recipe/SpoonacularRecipeDatasource.java
+++ server/src/main/java/Parsing/Recipe/SpoonacularRecipeDatasource.java
@@ -2,7 +2,10 @@ package Parsing.Recipe;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -32,18 +35,47 @@ public class SpoonacularRecipeDatasource {
   }
 
   /**
-   * Deserializes a raw instruction set json into RecipeInstructions object.
+   * Deserializes a raw instruction set json (for a meal - could be multiple sub-recipes) into MealInstructions object.
+   * NOTE: WILL NEED TO BE INTEGRATED INTO RECIPE OBJECT EVENTUALLY, MAY BECOME OBSOLETE.
+   *
+   * @param rawJson the raw meal instructions json to deserialize
+   * @return a MealInstructions, the deserialized version of rawJson
+   * @throws IOException if moshi is unable to deserialize rawJson into a MealInstructions object
+   * @throws IllegalArgumentException if rawJson describes a MealInstructions with no recipes or a recipe with no steps
+   */
+  public MealInstructions deserializeMealInstructions(String rawJson) throws IOException, IllegalArgumentException {
+    Type setOfString = Types.newParameterizedType(Set.class, String.class);
+    JsonAdapter<Set<String>> setOfStringAdapter = moshi.adapter(setOfString);
+    Set<String> rawSubRecipes = setOfStringAdapter.fromJson(rawJson);
+    if (rawSubRecipes.isEmpty()) {
+      throw new IllegalArgumentException("Error parsing: Given instructions json without any recipe instructions. Raw json: " + rawJson);
+    }
+
+    MealInstructions fullInstructionsDeserialized = new MealInstructions(new HashSet<>());
+    for (String rawSubRecipe : rawSubRecipes) {
+      RecipeInstructions instructions = instructionsAdapter.fromJson(rawSubRecipe);
+      if (instructions.steps.isEmpty()) {
+        throw new IllegalArgumentException("Error parsing: Given invalid instructions json where a recipe has no steps. Raw (sub)recipe json: " + rawSubRecipe);
+      } else {
+        fullInstructionsDeserialized.subRecipes.add(instructions);
+      }
+    }
+    return fullInstructionsDeserialized;
+  }
+
+  /**
+   * Deserializes a raw instruction set json (for a single recipe) into RecipeInstructions object.
    * NOTE: WILL NEED TO BE INTEGRATED INTO RECIPE OBJECT EVENTUALLY, MAY BECOME OBSOLETE.
    *
    * @param rawJson the raw instructions json to deserialize
-   * @return a Recipe, the deserialized version of rawJson
+   * @return a RecipeInstructions, the deserialized version of rawJson
    * @throws IOException if moshi is unable to deserialize rawJson into a RecipeInstructions object
-   * @throws IllegalArgumentException if rawJson describes a RecipeInstruction with no steps
+   * @throws IllegalArgumentException if rawJson describes a RecipeInstructions with no steps
    */
   public RecipeInstructions deserializeInstructions(String rawJson) throws IOException, IllegalArgumentException {
     RecipeInstructions instructions = instructionsAdapter.fromJson(rawJson);
     if (instructions.steps.isEmpty()) {
-      throw new IllegalArgumentException("Error parsing: Given invalid instructions json with no steps. Raw json: " + rawJson);
+      throw new IllegalArgumentException("Error parsing: Given invalid instructions json where >= 1 recipe has no steps. Raw json: " + rawJson);
     }
     return instructions;
   }
@@ -59,7 +91,8 @@ public class SpoonacularRecipeDatasource {
   /**
    * Inner record classes describing instructions (pertaining to a Recipe).
    */
-  public record RecipeInstructions(List<Step> steps) {}
+  public record MealInstructions(Set<RecipeInstructions> subRecipes) {}
+  public record RecipeInstructions(String name, List<Step> steps) {}
   public record Step(int number, Set<Equipment> equipment, Set<IngredientName> ingredients, String step) {}
   public record Equipment(String name) {}
   public record IngredientName(String name) {}
--- server/src/main/java/Parsing/SampleParsing.java
+++ server/src/main/java/Parsing/SampleParsing.java
@@ -1,6 +1,7 @@
 package Parsing;
 
 import Parsing.Recipe.SpoonacularRecipeDatasource;
+import Parsing.Recipe.SpoonacularRecipeDatasource.MealInstructions;
 import Parsing.Recipe.SpoonacularRecipeDatasource.Recipe;
 import Parsing.Recipe.SpoonacularRecipeDatasource.RecipeInstructions;
 import java.io.BufferedReader;
@@ -42,30 +43,31 @@ public class SampleParsing {
     System.out.println();
 
     /** PARSE INSTRUCTIONS */
-    System.out.println("PARSING EXAMPLE INSTRUCTIONS JSON ---------------------------------");
-    try {
-      // ***************** READING THE FILE *****************
-      FileReader jsonReader = new FileReader("data/exampleInstructions.json");
-      BufferedReader br = new BufferedReader(jsonReader);
-      String fileString = "";
-
-      System.out.println("Beginning to read file.");
-      String line = br.readLine();
-      while (line != null) {
-        fileString = fileString + line;
-        line = br.readLine();
-      }
-      jsonReader.close();
-      System.out.println("Done reading file.\n");
-
-      // ****************** DESERIALIZING *******************
-      System.out.println("Beginning to deserialize read String.");
-//      RecipeInstructions deserializedInstructions = dataSrc.deserializeInstructions(fileString);
-      System.out.println("Done deserializing read String.\n");
-
-    } catch (IOException | IllegalArgumentException e) {
-      System.out.println(e.getMessage());
-    }
-    System.out.println("END ---------------------------------------------------------");
+//    System.out.println("PARSING EXAMPLE INSTRUCTIONS JSON ---------------------------------");
+//    try {
+//      // ***************** READING THE FILE *****************
+//      FileReader jsonReader = new FileReader("data/exampleInstructions.json");
+////      FileReader jsonReader = new FileReader("data/exampleBasicInstructions.json");
+//      BufferedReader br = new BufferedReader(jsonReader);
+//      String fileString = "";
+//
+//      System.out.println("Beginning to read file.");
+//      String line = br.readLine();
+//      while (line != null) {
+//        fileString = fileString + line;
+//        line = br.readLine();
+//      }
+//      jsonReader.close();
+//      System.out.println("Done reading file.\n");
+//
+//      // ****************** DESERIALIZING *******************
+//      System.out.println("Beginning to deserialize read String.");
+//      MealInstructions deserializedInstructions = dataSrc.deserializeMealInstructions(fileString);
+//      System.out.println("Done deserializing read String.\n");
+//
+//    } catch (IOException | IllegalArgumentException e) {
+//      System.out.println(e.getMessage());
+//    }
+//    System.out.println("END ---------------------------------------------------------");
   }
 }
