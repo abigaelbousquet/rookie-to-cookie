2d47e990c4469bf301732c1a0ce98e90e1c252d4
marissa-kate <rissa6204@gmail.com>
2024-04-19 19:05:58 -0400

    feat: create account has slider, dropdown selects

--- client/src/components/Login/AccountCreation.tsx
+++ client/src/components/Login/AccountCreation.tsx
@@ -1,38 +1,99 @@
 // AccountCreationPopup.tsx
-import React, { useState } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import Select from "react-select";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { ControlledInput } from "./ControlledInput";
+import Creatable from "react-select/creatable";
 
 export function AccountCreation() {
   const [name, setName] = useState("");
+  const [exp, setExp] = useState("");
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const heightMarks = {
+    1: "novice",
+    2: "beginner",
+    3: "moderate",
+    4: "master",
+  };
+  const diets = [
+    { label: "Vegetarian", value: "Vegetarian" },
+    { label: "Vegan", value: "Vegan" },
+    { label: "Gluten-Free", value: "Gluten-Free" },
+    { label: "Dairy-Free", value: "Dairy-Free" },
+    { label: "Pescatarian", value: "Pescatarian" },
+    { label: "None", value: "None" },
+  ];
+  let allergies = [
+    { label: "Shellfish", value: "Shellfish" },
+    { label: "Egg", value: "Egg" },
+    { label: "Peanut", value: "Peanut" },
+    { label: "Nut", value: "Nut" },
+    { label: "Soy", value: "Soy" },
+    { label: "Sesame", value: "Sesame" },
+  ];
+  const handleSubmit = (props: string[]) => {
     // Handle form submission here, e.g., send data to server
-    console.log("Form submitted:", name);
+    console.log("Form submitted:", props);
   };
 
   return (
     <div className="popup">
       <div className="popup-inner">
-        <div className="popup-header">
-          <div id="account-header">
-            <h2>Create Account</h2>
+        <div className="popup-inmost">
+          <div className="popup-header">
+            <div id="account-header">
+              <h2>Create Account</h2>
+            </div>
+            <button className="x-button">X</button>
           </div>
-          <button className="x-button">X</button>
+          <div className="form">
+            <div>
+              <legend>Name:</legend>
+              <ControlledInput
+                value={name}
+                setValue={setName}
+                ariaLabel={"Name"}
+                placeholder={"Nim Telson"}
+                styleID={"input-box"}
+              />
+            </div>
+            <div>
+              <legend>Cooking Experience:</legend>
+              <Slider
+                defaultValue={1}
+                min={1}
+                max={4}
+                onChange={(e) => setExp(e.toString())}
+                marks={heightMarks}
+              />
+            </div>
+            <div>
+              <legend>Diet:</legend>
+              <Select
+                options={diets}
+                isMulti
+                onChange={(opt) => console.log(opt)}
+              />
+            </div>
+            <div>
+              <legend>Allergies & Foods to Exclude:</legend>
+              <Creatable
+                options={allergies}
+                isMulti
+                onChange={(opt, meta) => console.log(opt, meta)}
+              />
+            </div>
+
+            <button
+              className="butt"
+              onClick={() => handleSubmit([name, exp])}
+              type="submit"
+            >
+              Create Account
+            </button>
+          </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <legend>Name:</legend>
-            <ControlledInput
-              value={name}
-              setValue={setName}
-              ariaLabel={"Name"}
-              placeholder={"Nim Telson"}
-              styleID={"input-box"}
-            />
-          </div>
-          <button type="submit">Create Account</button>
-        </form>
       </div>
     </div>
   );
