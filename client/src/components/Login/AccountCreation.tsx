16f4deb29ef56ffd620879a899a3053108f9021e
marissa-kate <rissa6204@gmail.com>
2024-04-20 15:23:25 -0400

    feat: firebase query to backend, intolerances and diet list updated

--- client/src/components/Login/AccountCreation.tsx
+++ client/src/components/Login/AccountCreation.tsx
@@ -2,14 +2,23 @@
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import Select from "react-select";
+
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { ControlledInput } from "./ControlledInput";
 import Creatable from "react-select/creatable";
+interface profileProps {
+  name: string;
+  exp: string;
+  diet: string[];
+  intolerances: string[];
+}
 
 export function AccountCreation() {
   const [name, setName] = useState("");
   const [exp, setExp] = useState("");
-
+  const [diet, setDiet] = useState([""]);
+  const [allergen, setAllergen] = useState([""]);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
   const heightMarks = {
     1: "novice",
     2: "beginner",
@@ -19,21 +28,34 @@ export function AccountCreation() {
   const diets = [
     { label: "Vegetarian", value: "Vegetarian" },
     { label: "Vegan", value: "Vegan" },
-    { label: "Gluten-Free", value: "Gluten-Free" },
-    { label: "Dairy-Free", value: "Dairy-Free" },
-    { label: "Pescatarian", value: "Pescatarian" },
-    { label: "None", value: "None" },
+    { label: "Pescetarian", value: "Pescetarian" },
+    { label: "Paleo", value: "Paleo" },
+    { label: "Primal", value: "Primal" },
   ];
-  let allergies = [
+  let intolerance = [
     { label: "Shellfish", value: "Shellfish" },
     { label: "Egg", value: "Egg" },
     { label: "Peanut", value: "Peanut" },
     { label: "Nut", value: "Nut" },
     { label: "Soy", value: "Soy" },
     { label: "Sesame", value: "Sesame" },
+    { label: "Tree nut", value: "Tree nut" },
+    { label: "Sulfite", value: "Sulfite" },
+    { label: "Dairy", value: "Dairy" },
+    { label: "Gluten", value: "Gluten" },
   ];
-  const handleSubmit = (props: string[]) => {
+  const handleSubmit = async (props: profileProps) => {
     // Handle form submission here, e.g., send data to server
+    await fetch(
+      "http://localhost:3232/name=" +
+        props.name +
+        "&exp=" +
+        props.exp +
+        "&diet=" +
+        props.diet.toString() +
+        "&intolerances=" +
+        props.intolerances.toString()
+    );
     console.log("Form submitted:", props);
   };
 
@@ -73,13 +95,16 @@ export function AccountCreation() {
               <Select
                 options={diets}
                 isMulti
-                onChange={(opt) => console.log(opt)}
+                onChange={() => {
+                  console.log(allergen);
+                  (opt) => setAllergen(opt.map((tag) => tag.label));
+                }}
               />
             </div>
             <div>
-              <legend>Allergies & Foods to Exclude:</legend>
+              <legend>Intolerances:</legend>
               <Creatable
-                options={allergies}
+                options={intolerance}
                 isMulti
                 onChange={(opt, meta) => console.log(opt, meta)}
               />
@@ -87,7 +112,14 @@ export function AccountCreation() {
 
             <button
               className="butt"
-              onClick={() => handleSubmit([name, exp])}
+              onClick={() =>
+                handleSubmit({
+                  name: name,
+                  exp: exp,
+                  diet: diet,
+                  intolerances: allergen,
+                })
+              }
               type="submit"
             >
               Create Account
