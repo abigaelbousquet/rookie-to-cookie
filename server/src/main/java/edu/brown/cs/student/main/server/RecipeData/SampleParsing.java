7dc32365b0f1fd8839bc69c0b386b40be1622d11
abigaelbousquet <truthbousquet@gmail.com>
2024-04-25 10:08:43 -0400

    test: added some unit tests for recipe, recipe utilities, and recipe datasources

---
+++ server/src/main/java/edu/brown/cs/student/main/server/RecipeData/SampleParsing.java
@@ -0,0 +1,44 @@
+package edu.brown.cs.student.main.server.RecipeData;
+
+import edu.brown.cs.student.main.server.RecipeData.Datasource.DatasourceException;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.RecipeUtilities;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.SearchResult;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.SpoonacularRecipeSource;
+import edu.brown.cs.student.main.server.RecipeData.Recipe.Recipe;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class SampleParsing {
+
+  /**
+   * Main method for sample deserialization of example recipe and instruction set jsons (in .json
+   * file form).
+   *
+   * @param args n/a
+   */
+  public static void main(String[] args) {
+    /** QUERYING SPOONACULAR API */
+        try {
+          SpoonacularRecipeSource spoonacular = new SpoonacularRecipeSource();
+          List<Recipe> results = spoonacular.queryRecipes(2, null, null, null, null, null, null,
+     60);
+          SearchResult resultsObject = new SearchResult(2, results.size(), results);
+
+
+          // ********************* SAVING ***********************
+          System.out.println("Beginning to save read String.");
+          FileWriter jsonWriter = new FileWriter("data/exampleSearchResultLength2.json");
+          BufferedWriter bw = new BufferedWriter(jsonWriter);
+          String serializedResults = RecipeUtilities.SEARCH_RESULT_JSON_ADAPTER.toJson(resultsObject);
+          bw.write(serializedResults);
+          bw.close();
+          jsonWriter.close();
+          System.out.println("Done saving read String.\n");
+        } catch (IOException | IllegalArgumentException | DatasourceException e) {
+          System.out.println(e.getMessage());
+        }
+  }
+
+}
--- server/src/test/java/edu/brown/cs/student/main/server/TestRecipeDatasource.java
+++ server/src/test/java/edu/brown/cs/student/main/server/TestRecipeDatasource.java
@@ -1,13 +1,70 @@
 package edu.brown.cs.student.main.server;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import edu.brown.cs.student.main.server.RecipeData.Datasource.DatasourceException;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.MockedRecipeSource;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.RecipeUtilities;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.SearchResult;
+import edu.brown.cs.student.main.server.RecipeData.Datasource.SpoonacularRecipeSource;
+import edu.brown.cs.student.main.server.RecipeData.Recipe.Recipe;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 /**
  * A class for unit testing the RecipeDatasources (Mocked and Real).
  */
 public class TestRecipeDatasource {
-    /*
+
+  /**
+   * Tests querying mocked source.
+   */
   @Test
-  public void test
-     */
+  public void testMockedQuery() {
+    List<Recipe> mockedResult = null;
+    try {
+      // ***************** READING THE FILE *****************
+      FileReader jsonReader = new FileReader("data/exampleSearchResultLength2.json");
+      BufferedReader br = new BufferedReader(jsonReader);
+      String fileString = "";
+
+      String line = br.readLine();
+      while (line != null) {
+        fileString = fileString + line;
+        line = br.readLine();
+      }
+      jsonReader.close();
+
+      // ****************** DESERIALIZING *******************
+      mockedResult = RecipeUtilities.deserializeSearchResult(fileString).getResults();
+      assertEquals(2, mockedResult.size());
+
+    } catch (IOException | IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+      fail();
+    }
+
+    MockedRecipeSource mockedSrc = new MockedRecipeSource(mockedResult);
+
+    // same mocked result regardless of query parameters
+    assertEquals(mockedResult, mockedSrc.queryRecipes(2, null, null, null, null, null, null, 60));
+    assertEquals(mockedResult, mockedSrc.queryRecipes(5, "american", null, null, "dairy", null, null, 10));
+  }
+
+  /**
+   * Tests error handling of an invalid number of recipe param in query.
+   *
+   * @throws DatasourceException if another unexpected exception is thrown
+   */
+  @Test
+  public void testInvalidQuery() throws DatasourceException {
+    SpoonacularRecipeSource src = new SpoonacularRecipeSource();
+    assertThrows(IllegalArgumentException.class, () -> src.queryRecipes(-1, null, null, null, null, null, null, 60));
+    assertThrows(IllegalArgumentException.class, () -> src.queryRecipes(150, null, null, null, null, null, null, 60));
+  }
 }
