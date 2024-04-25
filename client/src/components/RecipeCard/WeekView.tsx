e2512b409caa5327ec9dcb0d3d5c62130e4fa85b
danidedona <daniela_dedona@brown.edu>
2024-04-25 18:07:52 -0400

    feat: new folder for calendar view

--- client/src/components/RecipeCard/WeekView.tsx
+++ client/src/components/RecipeCard/WeekView.tsx
@@ -1,1 +1,34 @@
+import React, { useState } from "react";
+import RecipeCard from "./RecipeCard";
+import "../../styles/WeekView.css";
 
+const WeekView = ({ mealPlan }) => {
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+
+  return (
+    <div className="week-calendar">
+      <div className="top-grid">
+        {/* Render week name cells */}
+        <div className="left-week-name-grid-cell">Sunday</div>
+        <div className="week-name-grid-cell">Monday</div>
+        <div className="week-name-grid-cell">Tuesday</div>
+        <div className="week-name-grid-cell">Wednesday</div>
+        <div className="week-name-grid-cell">Thursday</div>
+        <div className="week-name-grid-cell">Friday</div>
+        <div className="right-week-name-grid-cell">Saturday</div>
+      </div>
+      <div className="bottom-grid">
+        {/* Render recipe cells */}
+        {mealPlan.map(({ day, recipeExists, recipe }) => (
+          <div key={day} className="grid-cell">
+            {recipeExists && (
+              <RecipeCard recipe={recipe} setShowPopup={setShowPopup} />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WeekView;
