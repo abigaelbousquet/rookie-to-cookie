5ed89a9981db329e6ab85862e014da9a3afc29e4
danidedona <daniela_dedona@brown.edu>
2024-04-24 22:10:27 -0400

    feat: calendar page now loads bug: dates are not shown correctly

--- client/src/components/RecipeCard/Recipe.tsx
+++ client/src/components/RecipeCard/Recipe.tsx
@@ -2,6 +2,11 @@ interface Recipe {
   name: string;
   cuisine: string;
   instructions: string;
+  time: number;
 }
 
 export default Recipe;
+
+export function getRecipe(recipe) {
+  return recipe.id;
+}
