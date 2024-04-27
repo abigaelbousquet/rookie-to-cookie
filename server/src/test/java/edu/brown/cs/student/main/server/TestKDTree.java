3235dbccbdcf1c0bb301bf9c1b50428110c15a97
abigaelbousquet <truthbousquet@gmail.com>
2024-04-27 17:59:48 -0400

    test: added more tests for KDtree and n nearest neighbor implementation

--- server/src/test/java/edu/brown/cs/student/main/server/TestKDTree.java
+++ server/src/test/java/edu/brown/cs/student/main/server/TestKDTree.java
@@ -10,7 +10,9 @@ import edu.brown.cs.student.main.server.RecommenderAlgorithm.KDTree.RecipeRecomm
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -48,8 +50,11 @@ public class TestKDTree {
     return mockedResult;
   }
 
+  /**
+   * Tests expected creation of K-D tree from 2 Recipes.
+   */
   @Test
-  public void testTreeCreation() {
+  public void testTreeCreation2Nodes() {
     // set up data
     List<Recipe> recipes = parseMockedRecipes("data/exampleSearchResultLength2.json");
     assertEquals(2, recipes.size());
@@ -66,12 +71,120 @@ public class TestKDTree {
     assertEquals(2, tree.getSize());
 
     // node1 has location [0, 13, 5], node2 has location [3, 19, 18]
-    assertEquals(node2, tree.getRoot().right);
+    assertEquals(node2, tree.getRoot().getRight());
   }
 
-  // TODO: test creation with more nodes
+  /**
+   * Tests expected creation of K-D tree from 4 Recipes.
+   */
+  @Test
+  public void testTreeCreation4Nodes() {
+    // set up data
+    List<Recipe> recipes = parseMockedRecipes("data/exampleSearchResultLength4.json");
+    assertEquals(4, recipes.size());
+    RecipeNode node1 = new RecipeNode(recipes.get(0));
+    RecipeNode node2 = new RecipeNode(recipes.get(1));
+    RecipeNode node3 = new RecipeNode(recipes.get(2));
+    RecipeNode node4 = new RecipeNode(recipes.get(3));
 
-  // TODO: test 2 nearest neighbors maybe where size of tree is > 2, small example with mocked data
+    // initialize tree
+    RecipeRecommendationKDTree tree = new RecipeRecommendationKDTree();
+    tree.insert(recipes.get(3));
+    assertEquals(1, tree.getSize());
+    assertEquals(node4, tree.getRoot());
 
-  // TODO: test where tree doesn't contain n nodes, check returns all nodes
+    // node1 has location [0,11,6], node2 has location [0,7,5]
+    // node3 has location [0,9,7], node4 has location [1,13,8]
+
+    tree.insert(recipes.get(1)); // compares on 1st coordinate of location: numCuisine
+    assertEquals(2, tree.getSize());
+    assertEquals(node2, tree.getRoot().getLeft());
+
+    tree.insert(recipes.get(2)); // will go left on numCuisine, then right on numIngredients
+    assertEquals(3, tree.getSize());
+    assertEquals(node3, tree.getRoot().getLeft().getRight());
+
+    /*
+                node4
+            node2
+               node3
+             node1
+    */
+
+    tree.insert(recipes.get(0)); // will go L on numCuisine, R on numIngredients, L on numSteps
+    assertEquals(4, tree.getSize());
+    assertEquals(node1, tree.getRoot().getLeft().getRight().getLeft());
+  }
+
+  /**
+   * Tests nearest n neighbors search where tree contains less than n nodes.
+   */
+  @Test
+  public void testAllNeighborsClosest() {
+    List<Recipe> allRecipes = parseMockedRecipes("data/exampleSearchResultLength2.json");
+    assertEquals(2, allRecipes.size());
+
+    Recipe likedRecipe = parseMockedRecipes("data/exampleSearchResultLength4.json").get(0);
+
+    RecipeRecommendationKDTree tree = new RecipeRecommendationKDTree();
+    tree.insert(allRecipes.get(0));
+    tree.insert(allRecipes.get(1));
+
+    Set<Recipe> allRecipeSet = new HashSet<>(allRecipes);
+    List<Recipe> nearest5NeighborsList = tree.nearestNeighbors(likedRecipe, 5);
+    Set<Recipe> nearest5NeighborsSet = new HashSet<>(nearest5NeighborsList);
+    assertEquals(2, nearest5NeighborsSet.size());
+    assertEquals(allRecipeSet, nearest5NeighborsSet);
+  }
+
+  /**
+   * Tests that when targetNode is in the tree, it is returned as the closest neighbor to itself.
+   */
+  @Test
+  public void testSelfClosestNeighbor() {
+    List<Recipe> allRecipes = parseMockedRecipes("data/exampleSearchResultLength2.json");
+    assertEquals(2, allRecipes.size());
+
+    Recipe likedRecipe = allRecipes.get(0);
+
+    RecipeRecommendationKDTree tree = new RecipeRecommendationKDTree();
+    tree.insert(allRecipes.get(0));
+    tree.insert(allRecipes.get(1));
+
+    List<Recipe> nearest1NeighborList = tree.nearestNeighbors(likedRecipe, 1);
+    assertEquals(1, nearest1NeighborList.size());
+    assertEquals(likedRecipe, nearest1NeighborList.get(0));
+  }
+
+  /**
+   * Tests n nearest neighbor search where n > 1 and size of tree > n.
+   */
+  @Test
+  public void testNearestNeighborsBigTree() {
+    List<Recipe> allRecipes = parseMockedRecipes("data/exampleSearchResultLength4.json");
+    assertEquals(4, allRecipes.size());
+
+    // initialize tree
+    RecipeRecommendationKDTree tree = new RecipeRecommendationKDTree();
+    tree.insert(allRecipes.get(3));
+    tree.insert(allRecipes.get(1)); // compares on 1st coordinate of location: numCuisine
+    tree.insert(allRecipes.get(2)); // will go left on numCuisine, then right on numIngredients
+    tree.insert(allRecipes.get(0)); // will go L on numCuisine, R on numIngredients, L on numSteps
+    /*
+                node4
+            node2
+               node3
+             node1
+
+      node1 has location [0,11,6], node2 has location [0,7,5]
+      node3 has location [0,9,7], node4 has location [1,13,8]
+    */
+
+    Recipe likedRecipe = parseMockedRecipes("data/exampleSearchResultLength2.json").get(0);
+    List<Recipe> nearest2Neighbors = tree.nearestNeighbors(likedRecipe, 2);
+    assertEquals(2, nearest2Neighbors.size());
+    assertEquals(allRecipes.get(0), nearest2Neighbors.get(0));
+    assertEquals(allRecipes.get(3), nearest2Neighbors.get(1));
+  }
+
 }
