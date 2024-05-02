f5ebc040582d5c3569417d3063abaa133990f3fe
Abigael Bousquet <115128176+abigaelbousquet@users.noreply.github.com>
2024-05-02 19:40:20 -0400

    style: automatic styling by mvn package

--- server/src/main/java/edu/brown/cs/student/main/server/storage/MockedFirebase.java
+++ server/src/main/java/edu/brown/cs/student/main/server/storage/MockedFirebase.java
@@ -5,25 +5,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class MockedFirebase implements StorageInterface{
-    @Override
-    public void addDocument(String uid, String collection_id, String doc_id, Map<String, Object> data) {
-        System.out.println("added document" + doc_id);
-    }
+public class MockedFirebase implements StorageInterface {
+  @Override
+  public void addDocument(
+      String uid, String collection_id, String doc_id, Map<String, Object> data) {
+    System.out.println("added document" + doc_id);
+  }
 
-    @Override
-    public List<Map<String, Object>> getCollection(String uid, String collection_id) throws InterruptedException, ExecutionException {
-        System.out.println("got collection" + collection_id);
-        return new ArrayList<>();
-    }
+  @Override
+  public List<Map<String, Object>> getCollection(String uid, String collection_id)
+      throws InterruptedException, ExecutionException {
+    System.out.println("got collection" + collection_id);
+    return new ArrayList<>();
+  }
 
-    @Override
-    public void clearUser(String uid) throws InterruptedException, ExecutionException {
-        System.out.println("cleared user");
-    }
+  @Override
+  public void clearUser(String uid) throws InterruptedException, ExecutionException {
+    System.out.println("cleared user");
+  }
 
-    @Override
-    public void clearRecipes(String uid, String path, String recipeId) throws IllegalArgumentException {
-        System.out.println("cleared recipes");
-    }
+  @Override
+  public void clearRecipes(String uid, String path, String recipeId)
+      throws IllegalArgumentException {
+    System.out.println("cleared recipes");
+  }
 }
